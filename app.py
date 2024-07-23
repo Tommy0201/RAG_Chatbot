5167630d623d1ac2939cfca793ff2ebf010b5a83
Tommy0201 <ng.phucminh04@gmail.com>
2024-07-23 23:42:18 +0700

    fixing app.py so that it take in database from frontend

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 FLASK_RUN_HOST = os.environ.get('FLASK_RUN_HOST', "localhost")
 FLASK_RUN_PORT = os.environ.get('FLASK_RUN_PORT', "8000")
+
+UPLOAD_FOLDER = "uploads"
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +14,23 @@ CORS(app)
 @app.route('/')
 def index():
     return "HellOOOO"
+
+@app.route('/upload_file', methods=["POST"])
+def upload_file():
+    if 'files' not in request.files:
+        return jsonify({"message": "No files part"}), 400
+    files = request.files.getlist('files')
+    file_paths = []
+
+    for file in files:
+        if file.filename == '':
+            return jsonify({"message": "No selected file"}), 400
+        
+        file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(file_path)
+        file_paths.append(file_path)
+
+    return jsonify({"message": "Files successfully uploaded", "files": file_paths}),200
 
 if __name__ == "__main__":
     app.run(host=FLASK_RUN_HOST,port=FLASK_RUN_PORT)
